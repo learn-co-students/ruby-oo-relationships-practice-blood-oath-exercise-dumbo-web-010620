259120358952ed8b90ff08d689e9c5ba5341f0c6
Jin Wu <jinn.wuu@gmail.com>
2020-01-10 11:08:28 -0500

    lab started

---
+++ app/models/cult.rb
@@ -0,0 +1,11 @@
+class Cult
+
+    attr_accessor :name, :location, :founding_year
+
+    def initialize(name, location, founding_year)
+        @name = name
+        @location = location
+        @founding_year = founding_year
+    end
+
+end
---
+++ app/models/follower.rb
@@ -0,0 +1,4 @@
+class Follower
+
+
+end
